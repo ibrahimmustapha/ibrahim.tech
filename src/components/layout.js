f18c5bbe5051e643ca48680da5f8a99ce846bdd7
ibrahimmustapha <musib581@gmail.com>
2022-01-31 13:52:35 +0000

    search ui-updated

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
+import Theme from "./min__components/theme"
 import {
 container,
 heading,
@@ -28,9 +29,10 @@ const Layout = ({ pageTitle, children }) => {
             <h2 className={heading}>{pageTitle}</h2>
             <nav>
                 <ul className={navLinks}>
-                <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
-                <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
-                <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
+                <li className={navLinkItem}><Link to="/" className={navLinkText}>home</Link></li>
+                <li className={navLinkItem}><Link to="/about" className={navLinkText}>about</Link></li>
+                <li className={navLinkItem}><Link to="/blog" className={navLinkText}>blog</Link></li>
+                <Theme />
                 </ul>
             </nav>
             <main>
